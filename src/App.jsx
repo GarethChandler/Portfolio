f7c6c6493ac840986539c76cd7b28cd3c5ba5efb
Gareth Chandler <gchand05@me.com>
2025-05-22 21:06:58 +1000

    Add routing but not implemented. Added new blank page for project details

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css";
+import { Routes, Route } from "react-router-dom";
 import About from "./components/sections/about";
 import Projects from "./components/sections/projects";
 import Contact from "./components/sections/contact";
 import Hero from "./components/sections/hero";
 import Header from "./components/sections/header";
 import Timeline from "./components/sections/timeline";
+import ProjectDetail from "./pages/projectDetail";
 
 function App() {
   // const [isLoaded, setIsLoaded] = useState(false);
